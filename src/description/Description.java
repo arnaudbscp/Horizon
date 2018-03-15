2283b36ab657b9de3d15f5854fd0b63084df6fe1
outtier samuel <samuel.o@outlook.fr>
2018-03-15 16:38:38 +0100

    Modification effectuée par Samuel à 16:38

--- src/description/Description.java
+++ src/description/Description.java
@@ -3,5 +3,6 @@ package description;
 public class Description {
 	public static void main(String[] args) {
 		System.out.println("Bonjour, c'est Jean-Michel.");
+		System.out.println("J'ai 56 ans et j'ai un compte Meetic.");
 	}
 }
