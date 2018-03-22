184897ce460476cc92d908b55e4732a921e54ff0
BASCOP Arnaud <arnaud.bascop@univ-lille1.fr>
2018-03-22 11:35:54 +0100

    Description nue

--- src/description/Description.java
+++ src/description/Description.java
@@ -1,9 +1,25 @@
 package description;
 
+
 public class Description {
 	
 	public Description() {
 		
 	}
 	
+	public Tache getTacheById(String id) {
+		
+	}
+	
+	public Tache getDebut() {
+		
+	}
+	
+	public Tache getFin() {
+		
+	}
+	
+	public Couleur getRandom() {
+		
+	}
 }
