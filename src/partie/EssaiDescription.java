b468d33a4b2f2290402f74107ea9d802eb73f7a6
Benji <benjamin.florentin@isen.yncrea.fr>
2018-03-23 22:23:04 +0100

    test unitaire

--- src/partie/EssaiDescription.java
+++ src/partie/EssaiDescription.java
@@ -1,12 +1,19 @@
 package partie;
 
+import description.Alea;
+import description.Couleur;
 import description.Description;
 import description.Tache;
+import description.Tacheclass;
 public class EssaiDescription {
 
 	public static void main(String[] args) {
 		Description d= new Description();
+		Tache a= d.getTacheById("1");
+		System.out.println(a.getId());
+		System.out.println(a.getDescription());
+		Alea b =a.getAlea(Couleur.JAUNE);
+		System.out.println(b);
 		
-		System.out.println(d.getTacheById("1").getId());
 	}
 }
