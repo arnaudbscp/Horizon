f6849b330b100d838f6573f55a870fcf514c47d8
outtier samuel <samuel.o@outlook.fr>
2018-04-05 14:14:26 +0200

    Ajout de la classe instanciable DonneesJoueurs

---
+++ src/partie/DonneesJoueurs.java
@@ -0,0 +1,52 @@
+package partie;
+
+import java.util.ArrayList;
+
+public class DonneesJoueurs {
+	private int montantCaisse;
+	private int qualiteProduit; 
+	private ArrayList<Realisation> liste;
+	private String nom;
+	
+	public void actualisation(int temps) {
+		
+	}
+	
+	public void baisseQualite(int delta) {
+		
+	}
+	
+	public void depense(int somme) {
+		
+	}
+	
+	public int getCaisse() {
+		return this.montantCaisse;
+	}
+	
+	public Graphe getGraphe() {
+		
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public int getQualite() {
+		return this.qualiteProduit;
+	}
+	
+	public Realisation getRealisation(String id) {
+		
+	}
+	
+	public Strategie getStrategie() {
+		
+	}
+	
+	public void uneSemaine() {
+		
+	}
+	
+	
+}
