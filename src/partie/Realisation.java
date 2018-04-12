b29c307ecf194ffff00044ecc9981fb2b708da19
florentb <benji97438@outlook.fr>
2018-04-12 11:44:40 +0200

    realisation

---
+++ src/partie/Realisation.java
@@ -0,0 +1,18 @@
+package partie;
+
+public class Realisation {
+
+	private String id;
+	private String nomEquipe;
+	private boolean[] protec;
+	private boolean acceleration=false;
+	public Realisation(String id, String nom) {
+		this.id=id;
+		this.nomEquipe=nom;
+		protec= new boolean[3];
+	}
+	public void setProtection() {
+		
+		
+	}
+}
