06351d37776c35d1799036ce62e4f2914bdcdade
Benji <benjamin.florentin@isen.yncrea.fr>
2018-05-11 12:00:18 +0200

    Modif Réalisation (fonction avancer pour modifier l'etat de la tache )

---
+++ src/partie/tetst.java
@@ -0,0 +1,23 @@
+package partie;
+
+import description.Description;
+
+public class tetst {
+
+	public static void main(String[] args) {
+		
+		final String NOM_JOUEUR = "NOM_JOUEUR";
+		Description description;
+		
+		DonneesJoueur donneesJoueur;
+		description = new Description();
+		Partie partie= new Partie(description,"NOM_JOUEUR");
+		donneesJoueur = (DonneesJoueur) partie.getVueJoueur(NOM_JOUEUR);
+		VueJoueur v =partie.getVueJoueur(NOM_JOUEUR);
+		if(v == null) {
+			System.out.println("1");
+		}
+		System.out.println(v.getCaisse());
+		
+	}
+}
